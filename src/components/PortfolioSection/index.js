95298caab5983d9cbb19e757d540087ee92c864f
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-21 14:04:49 -0300

    Create PortfolioSection

---
+++ src/components/PortfolioSection/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section } from './elements'
+
+const PortfolioSection = () => {
+  return (
+    <Section>
+      
+    </Section>
+  )
+}
+
+export default PortfolioSection
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import AboutSection from "./AboutSection";
 import HeroSection from "./HeroSection";
+import PortfolioSection from "./PortfolioSection";
 import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
-export { AboutSection, HeroSection, NavBar, SideBar };
+export { AboutSection, HeroSection, PortfolioSection, NavBar, SideBar };
