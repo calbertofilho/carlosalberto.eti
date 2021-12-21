2b480f012bc22632fe1084448d1469bb4f89458a
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-21 14:08:13 -0300

    Create DownloadsSection

---
+++ src/components/DownloadsSection/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section } from './elements'
+
+const DownloadsSection = () => {
+  return (
+    <Section>
+      
+    </Section>
+  )
+}
+
+export default DownloadsSection
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import AboutSection from "./AboutSection";
+import DownloadsSection from "./DownloadsSection";
 import HeroSection from "./HeroSection";
 import PortfolioSection from "./PortfolioSection";
 import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
-export { AboutSection, HeroSection, PortfolioSection, NavBar, SideBar };
+export { AboutSection, DownloadsSection, HeroSection, PortfolioSection, NavBar, SideBar };
