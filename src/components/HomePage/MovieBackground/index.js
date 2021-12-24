c149e62d102b89196997e314ff590f3d33a56048
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-24 08:56:22 -0300

    Update logic on movie change in Movie Background component

--- src/components/HomePage/MovieBackground/index.js
+++ src/components/HomePage/MovieBackground/index.js
@@ -4,47 +4,40 @@ import Video1 from '../../../assets/videos/hero-page-1080p.mp4';
 import Video2 from '../../../assets/videos/production-1080p.mp4';
 
 const MovieBackground = () => {
-  document.getElementById('video1').style.display = 'block'
-  document.getElementById('video2').style.display = 'none'
+  const sectionVisibleIs = (section) => {
+    const sections = document.querySelectorAll('section');
+    let currentSection = '';
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+
+      if (window.pageYOffset >= sectionTop) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+  
+    return (currentSection === section);
+  };
 
   React.useEffect(() => {
     window.addEventListener('scroll', () => {
-      const sections = document.querySelectorAll('section');
-      let currentSection = '';
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-  
-        if (window.pageYOffset >= sectionTop) {
-          currentSection = section.getAttribute('id');
-        }
-      });
-      if (currentSection === 'home') {
-        document.getElementById('video1').style.display = 'block'
-        document.getElementById('video2').style.display = 'none'
-      } else {
-        document.getElementById('video1').style.display = 'none'
-        document.getElementById('video2').style.display = 'block'
-      }
+      var sourceCurrentVideo = document.getElementById('video').currentSrc;
+      var sourceVideo1 = `${Video1}`;
+      var sourceVideo2 = `${Video2}`;
+      var filenameCurrentVideo = sourceCurrentVideo.substring(sourceCurrentVideo.lastIndexOf("/") + 1, sourceCurrentVideo.length);
+      var filenameVideo1 = sourceVideo1.substring(sourceVideo1.lastIndexOf("/") + 1, sourceVideo1.length);
+      var filenameVideo2 = sourceVideo2.substring(sourceVideo2.lastIndexOf("/") + 1, sourceVideo2.length);
 
-      // var source = document.getElementById('video').currentSrc;
-      // var newSource1 = `${Video1}`;
-      // var newSource2 = `${Video2}`;
-      // var currentVideo = source.substring(source.lastIndexOf("/") + 1, source.length);
-      // var newVideo1 = newSource1.substring(newSource1.lastIndexOf("/") + 1, newSource1.length);
-      // var newVideo2 = newSource2.substring(newSource2.lastIndexOf("/") + 1, newSource2.length);
-
-      // if ((currentSection === 'home') && (currentVideo !== newVideo1))
-      //   document.getElementById('video1').src = `${Video1}`;
-      // else if ((currentSection !== 'home') && (currentVideo !== newVideo2))
-      //   document.getElementById('video2').src = `${Video2}`;
-
+      if (sectionVisibleIs('home') && (filenameCurrentVideo !== filenameVideo1))
+        document.getElementById('video').src = `${Video1}`;
+      else if (!sectionVisibleIs('home') && (filenameCurrentVideo !== filenameVideo2))
+        document.getElementById('video').src = `${Video2}`;
     });
   })
 
   return (
     <Background>
-      <VideoMovie id='video1' src={Video1} autoPlay loop muted />
-      <VideoMovie id='video2' src={Video2} autoPlay loop muted />
+      <VideoMovie id='video' src={Video1} autoPlay loop muted />
     </Background>
   )
 }
