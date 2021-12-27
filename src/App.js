1ac89cabdc07d71ca6e58192ceac4cf250cae570
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-27 12:35:12 -0300

    Update structure, add react-helmet to load head metatags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import HomePage from './pages/Home';
@@ -7,14 +8,24 @@ import ThanksPage from './pages/Thanks';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Switch>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/app' component={AppPage} exact />
-        <Route path='/thanks' component={ThanksPage} exact />
-      </Switch>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet htmlAttributes={{ lang : 'pt-br' }}>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-Language" content="pt-br" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Especialista em Tecnologia da Informação" />
+        <title>Carlos Alberto ETI</title>
+      </Helmet>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Switch>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/app' component={AppPage} exact />
+          <Route path='/thanks' component={ThanksPage} exact />
+        </Switch>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
