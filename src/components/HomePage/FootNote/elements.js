8835dd422b1fb119eb86128ae560e5a9c25dda8b
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-30 17:17:31 -0300

    Update FootNote, writing homepage footer content

--- src/components/HomePage/FootNote/elements.js
+++ src/components/HomePage/FootNote/elements.js
@@ -1,11 +1,89 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { IoLogoGithub, IoLogoVercel, IoLogoInstagram, IoLogoTwitter, IoLogoYoutube } from 'react-icons/io5';
+import { SiGmail } from 'react-icons/si';
+import { RiGlobalFill } from "react-icons/ri";
+import { Link as LinkScroll } from 'react-scroll';
 
 export const Content = styled.div`
-  z-index: 1;
   background: #01bf71;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
 `;
 
-export const Heading = styled.h1`
+export const InfoWrap = styled.div`
+  height: 100%;
+  width: 50%;
+  padding: 24px;
+  align-self: flex-end;
+`;
+
+export const SocialMedia = styled.div`
+  align-self: flex-start;
   color: #fff;
-  font-size: 14px;
+  margin-bottom: 40px;
+  font-size: 36px;
+`;
+
+export const Github = styled(IoLogoGithub)`
+  margin: 8px;
+`;
+
+export const Vercel = styled(IoLogoVercel)`
+  margin: 8px;
+`;
+
+export const Instagram = styled(IoLogoInstagram)`
+  margin: 8px;
+`;
+
+export const Twitter = styled(IoLogoTwitter)`
+  margin: 8px;
+`;
+
+export const Youtube = styled(IoLogoYoutube)`
+  margin: 8px;
+`;
+
+export const Gmail = styled(SiGmail)`
+  margin: 8px;
+`;
+
+export const Site = styled(RiGlobalFill)`
+  margin: 8px;
+`;
+
+export const Paragraph = styled.p`
+  color: #000;
+  font-size: 16px;
+`;
+
+export const LogoWrap = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+`;
+
+export const Logo = styled(LinkScroll)`
+  cursor: pointer;
+  color: #000;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  padding: 24px;
+  white-space: nowrap;
+  font-family: 'Reality Hyper TTF', sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+  text-transform: uppercase;
+`;
+
+export const LogoEmphasis = styled.span`
+  font-family: 'Reality Hyper TTF', sans-serif;
+  font-weight: bold;
+  font-size: 4.4rem;
+  margin-left: 12px;
 `;
--- src/components/HomePage/FootNote/index.js
+++ src/components/HomePage/FootNote/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Content, Heading } from './elements'
+import { Content, LogoWrap, Logo, LogoEmphasis, InfoWrap, Paragraph, SocialMedia, Github, Vercel, Instagram, Twitter, Youtube, Gmail, Site } from './elements'
 
 const FootNote = () => {
   return (
     <Content>
-      <Heading>
-        Só para testar, mesmo !!!
-      </Heading>
+      <InfoWrap>
+        <SocialMedia>
+          <Github />
+          <Vercel />
+          <Instagram />
+          <Twitter />
+          <Youtube />
+          <Gmail />
+          <Site />
+        </SocialMedia>
+        <Paragraph>Copyright &copy; { (new Date().getFullYear()) }  |  carlosalberto.eti.br &trade; 2006</Paragraph>
+      </InfoWrap>
+      <LogoWrap>
+        <Logo to='home' className='home'>Carlos<br />Alberto<LogoEmphasis>ETI</LogoEmphasis></Logo>
+      </LogoWrap>
     </Content>
   )
 }
