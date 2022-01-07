ecff2b7e3bb75f108e0fb24e71d42473b5d69870
Carlos Alberto Filho <calbertofilho@gmail.com>
2022-01-07 15:52:34 -0300

    Update FootNote, adapting to responsive mode

--- src/components/HomePage/FootNote/elements.js
+++ src/components/HomePage/FootNote/elements.js
@@ -9,29 +9,47 @@ export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  z-index: 1;
+
+  @media screen and (max-width: 710px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 export const InfoWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: left;
   height: 100%;
   width: 50%;
   padding: 24px;
-  align-self: flex-end;
+  flex-direction: column;
+
+  @media screen and (max-width: 852px) {
+    width: 60%;
+  }
+
+  @media screen and (max-width: 710px) {
+    width: 100%;
+    padding: 12px;
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
 export const SocialMedia = styled.div`
   position: relative;
-  display: table;
-  border-collapse: separate;
-  border-spacing: 12px;
   margin-bottom: 40px;
+
+  @media screen and (max-width: 400px) {
+    justify-content: center;
+  }
 `;
 
 const Links = css`
   cursor: pointer;
   color: #bdffe4;
   position: relative;
-  display: table-cell;
+  display: inline-block;
   vertical-align: middle;
   text-align: center;
   background: rgba(255, 255, 255, 0.2);
@@ -39,9 +57,9 @@ const Links = css`
   border-top: 1px solid rgba(255, 255, 255, 0.1);
   border-left: 1px solid rgba(255, 255, 255, 0.1);
   border-radius: 10px;
-  width: 40px;
-  height: 40px;
   overflow: hidden;
+  padding: 6px;
+  margin: 0 6px;
   box-shadow: -4px -4px 10px 0 rgba(0, 0, 0, 0.2);
   filter: blur(0.5px);
   transition: 0.5s all ease;
@@ -116,6 +134,7 @@ export const Site = styled(RiGlobalFill)`
 export const Paragraph = styled.p`
   color: #000;
   font-size: 16px;
+  text-align: center;
 `;
 
 export const LogoWrap = styled.div`
@@ -124,6 +143,15 @@ export const LogoWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: right;
+
+  @media screen and (max-width: 852px) {
+    width: 40%;
+  }
+
+  @media screen and (max-width: 710px) {
+    width: 100%;
+    justify-content: center;
+  }
 `;
 
 export const Logo = styled(LinkScroll)`
@@ -138,6 +166,18 @@ export const Logo = styled(LinkScroll)`
   font-weight: bold;
   text-decoration: none;
   text-transform: uppercase;
+
+  @media screen and (max-width: 270px) {
+    font-size: 1.25rem;
+  }
+
+  @media screen and (max-width: 250px) {
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 180px) {
+    font-size: 0.75rem;
+  }
 `;
 
 export const LogoEmphasis = styled.span`
@@ -145,4 +185,16 @@ export const LogoEmphasis = styled.span`
   font-weight: bold;
   font-size: 4.4rem;
   margin-left: 12px;
+
+  @media screen and (max-width: 270px) {
+    font-size: 4rem;
+  }
+
+  @media screen and (max-width: 250px) {
+    font-size: 3rem;
+  }
+
+  @media screen and (max-width: 180px) {
+    font-size: 2.5rem;
+  }
 `;
